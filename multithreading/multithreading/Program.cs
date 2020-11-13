3c73e963cdc2b6512ff71bb1c2274eb2f81d9440
Sofia <sofia.aveiro.silva@gmail.com>
2020-11-13 16:27:50 +0000

    Added small details

--- multithreading/multithreading/Program.cs
+++ multithreading/multithreading/Program.cs
@@ -1,6 +1,11 @@
 ﻿namespace multithreading
 {
     //https://www.pluralsight.com/guides/lock-statement-best-practices
+    // Threads and Multithreading:
+    // The process of multi-threading allows multiple tasks to execute concurrently, and to manipulate other tasks.
+    // We use threads to run something which does not need user interaction
+    // and which runs in parallel with the normal operations by the user.
+
     using System;
     using System.Threading;
 
@@ -8,66 +13,37 @@
     {
         static void Main()
         {
-            // construct two threads for our demonstration;  
-            Thread thread1 = new Thread(() => Print1("Twitter!"));
-            Thread thread2 = new Thread(() => Print2());
-            Thread thread3 = new Thread(() => Print3("Linkedin!"));
+            // Declare a SocialNetwork
+            SocialNetwork socialNetwork = new SocialNetwork();
 
-            // start them  
+            // initialize 3 threads class object 
+            Thread thread1 = new Thread(() => socialNetwork.Print1("Twitter!"));  // sends a parameter
+            Thread thread2 = new Thread(() => socialNetwork.Print2());            // doesn't send any parameter
+            Thread thread3 = new Thread(() => socialNetwork.Print3("Linkedin!")); // sends a parameter
+
+            // we can explicitly assign priority to a thread
+            // that doesn't guarantee us that the thread will the first or the last to be executed
+            // setting the thread priority to highest will not correspond to real-time execution
+            thread2.Priority = ThreadPriority.Highest;
+            thread1.Priority = ThreadPriority.Lowest;
+            thread3.Priority = ThreadPriority.Lowest;
+
+            // start running our threads  
             thread1.Start();
             thread2.Start();
             thread3.Start();
+
+            // this code is read at the same time as the threads are being executed
+            Console.WriteLine("threads are being executed!");
+
+            // wait until all threads are done with its execution.
+            thread1.Join();
+            thread2.Join();
+            thread3.Join();
+
+            // after executing the threads display message
+            Console.WriteLine("Press Enter to terminate!");
+            Console.ReadLine();
         }
-
-        private static void Print1(string socialNetwork)
-        {
-            lock (socialNetworkLock)
-            {
-                if (socialNetwork.Equals("Twitter!"))
-                {
-                    Console.WriteLine("Hello I received a message from " + socialNetwork);
-                }
-            }
-        }
-
-        private static void Print2()
-        {
-            if (!isInitialized)
-            {
-                // we should avoid locking on any public or even a variable that it's in use,
-                // we should only lock in a private variable of type object, for example, that it
-                // is meant to serve only this purpose
-                lock (socialNetworkLock)
-                {
-                    //checks the state of the application
-                    if (!isInitialized)
-                    {
-                        foreach (string network in socialNetworkList)
-                        {
-                            Thread.Sleep(1000);
-                            Console.WriteLine("Hello from " + network);
-                        }
-
-                        isInitialized = true;
-                    }
-                }
-            }
-        }
-
-        private static void Print3(string socialNetwork)
-        {
-            lock (socialNetworkLock)
-            {
-                if (socialNetwork.Equals("Linkedin!"))
-                {
-                    Console.WriteLine("Hello I received a message from " + socialNetwork);
-                }
-            }
-        }
-
-        private static bool isInitialized = false;
-        // we should lock always in a provate readonly instance of object:
-        private static readonly object socialNetworkLock = new object();
-        private static string[] socialNetworkList = new string[5] { "Snapchat", "Instagram", "Slack", "Linkedin", "WhatsApp" };
     }
 }
---
+++ multithreading/multithreading/SocialNetwork.cs
@@ -0,0 +1,64 @@
+﻿namespace multithreading
+{
+    using System;
+    using System.Threading;
+
+    public class SocialNetwork
+    {
+
+        public void Print1(string socialNetwork)
+        {
+            lock (socialNetworkLock)
+            {
+                if (socialNetwork.Equals("Twitter!"))
+                {
+                    Console.WriteLine("Hello I received a message from " + socialNetwork);
+                }
+            }
+        }
+
+        public void Print2()
+        {
+            if (!isInitialized)
+            {
+                // While a lock is held, the thread that holds the lock can again acquire and release the lock.
+                // Any other thread is blocked from acquiring the lock and waits until the lock is released.
+                // we should avoid locking on any public or even a variable that it's in use,
+                // we should only lock in a private variable of type object, for example, that it
+                // is meant to serve only this purpose
+                lock (socialNetworkLock)
+                {
+                    //checks the state of the application
+                    if (!isInitialized)
+                    {
+                        foreach (string network in socialNetworkList)
+                        {
+                            Thread.Sleep(500);
+                            Console.WriteLine("Hello from " + network);
+                        }
+
+                        isInitialized = true;
+                    }
+                }
+            }
+        }
+
+        public void Print3(string socialNetwork)
+        {
+            lock (socialNetworkLock)
+            {
+                if (socialNetwork.Equals("Linkedin!"))
+                {
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Hello I received a message from " + socialNetwork);
+                }
+            }
+        }
+
+        private bool isInitialized = false;
+        // we should lock always in a provate readonly instance of object:
+        private readonly object socialNetworkLock = new object();
+        private readonly string[] socialNetworkList = new string[5] { "Snapchat", "Instagram", "Slack", "Linkedin", "WhatsApp" };
+
+    }
+}
